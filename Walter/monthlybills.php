aa1ae88091afe26efd78b3f611a2fd15906bc216
Stribor45 <ikamzic@gmail.com>
2020-07-14 21:35:47 -0400

    July 14 2020

--- Walter/monthlybills.php
+++ Walter/monthlybills.php
@@ -1,19 +1,18 @@
 <?php
    
-   if (isset($_GET["my_submit"])) {
-      $rent = htmlspecialchars($_GET["rent_mortgage"]);
-      echo $rent;
-      $home = $_GET["home_phone"];
-      $income = $_GET["income"];
-      $cell = $_GET["cell_phone"];
-      $hydro = $_GET["hydro"];
-      $num_cell = $_GET["num_cell"];
-      $cable = $_GET["cable"];
-      $total = $_GET["total"];
-      $travel = $_GET["travel_expenses"];
-      $balance = $_GET["balance"];
-      $food = $_GET["food"];
-      $projected = $_GET["projected"];       
+   if (isset($_POST["my_submit"])) {
+      $rent = trim($_POST["rent_mortgage"]);
+      $home = trim($_POST["home_phone"]);
+      $income = trim($_POST["income"]);
+      $cell = trim($_POST["cell_phone"]);
+      $hydro = trim($_POST["hydro"]);
+      $num_cell = trim($_POST["num_cell"]);
+      $cable = trim($_POST["cable"]);
+      $total = trim($_POST["total"]);
+      $travel = trim($_POST["travel_expenses"]);
+      $balance = trim($_POST["balance"]);
+      $food = trim($_POST["food"]);
+      $projected = trim($_POST["projected"]);       
    }
    else {
       $rent = "";
