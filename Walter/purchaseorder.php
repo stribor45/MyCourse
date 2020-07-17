fb785409afbe52001e6c254913dd7b22aa2ebda6
Stribor45 <ikamzic@gmail.com>
2020-07-16 20:41:34 -0400

    All lessons until seq programming

---
+++ Walter/purchaseorder.php
@@ -0,0 +1,12 @@
+<?php
+
+    include ("Include/header.html");
+    include ("Include/navigation.html");
+    include ("Include/purchaseorder.html");
+    include ("Include/footer.html");
+
+
+?>
+
+
+
---
+++ Walter/receipt.php
@@ -0,0 +1,10 @@
+<?php
+
+    include ("Include/header.html");
+    include ("Include/navigation.html");
+    include ("Include/receipt.html");
+    include ("Include/footer.html");
+
+
+
+?>
