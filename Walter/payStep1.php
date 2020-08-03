123ec8c1b97c96a9319058a201cee026a70c6672
Stribor45 <ikamzic@gmail.com>
2020-08-03 16:49:46 -0400

    last updates added regular abd overtime

--- Walter/payStep1.php
+++ Walter/payStep1.php
@@ -2,13 +2,20 @@
 
 
     if (isset($_POST["my_submit"])) {
+
+        $step2 = 1; //if 1 output withing the form and if 0 output just to blank page
+        /*$f = new NumberFormatter("en", NumberFormatter::CURRENCY);
+        $wage = $f->formatCurrency($w, "USD"); 
+        $wage = $f->formatCurrency($wage, "en_CA"); 
+        $gross = $f->formatCurrency($gross, "en_CA"); */
+
         $overtime_hours = 0;
         $payroll_tax = 0;
         $first_name = trim($_POST["first_name"]);
         $last_name = trim($_POST["last_name"]);
         $hours = trim($_POST["num_hours"]);
         $wage = trim($_POST["wage"]);
-      
+       
         If ($hours > 40) {
             $reg_hours = 40;
             $overtime_hours = $hours - 40;
@@ -18,10 +25,11 @@
         }
 
         $regular = $reg_hours * $wage;
-        $overtime = $overtime_hours * ($wage *1.5);
+        $overtime = $overtime_hours * ($wage * 1.5);
         $gross = $regular + $overtime;
         $payroll_tax = $gross * 0.25;
         $net_pay = $gross - $payroll_tax;
+
     }
     else {
         $first_name = "";
